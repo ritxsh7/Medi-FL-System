18e22c5c73fa1f13d750ba2f95de2d5409b442d2
Ritesh <ritessshhh7@gmail.com>
2025-01-14 23:44:28 +0530

    add routes for auth

--- fl-app/client/src/App.jsx
+++ fl-app/client/src/App.jsx
@@ -3,12 +3,17 @@ import AdminUI from "./components/Admin";
 import ClientUI from "./components/Client";
 import Test from "./components/Test";
 import "./App.css";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+import Header from "./components/Header";
 
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
-        <Route exact path="/" element={<p>Welcome to MedFl</p>} />
+        <Route exact path="/" element={<LoginPage />} />
+        <Route exact path="/signup" element={<SignupPage />} />
         <Route exact path="/admin" element={<AdminUI />} />
         <Route exact path="/test" element={<Test />} />
         <Route exact path="/client/:id" element={<ClientUI />} />
