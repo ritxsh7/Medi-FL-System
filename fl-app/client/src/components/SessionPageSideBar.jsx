3e442c62504990c7ded95660806b5475fe2609fa
Ritesh <ritessshhh7@gmail.com>
2025-01-20 23:45:34 +0530

    add conditional status of session

--- fl-app/client/src/components/SessionPageSideBar.jsx
+++ fl-app/client/src/components/SessionPageSideBar.jsx
@@ -28,11 +28,13 @@ const SessionPageSideBar = ({
         </p>
         <p
           className={`${
-            session?.status === "pending"
+            session.status === "pending"
+              ? "text-red-500"
+              : session.status === "listening"
               ? "text-orange-500"
-              : session?.status === "in-progress"
-              ? "bg-blue-100 text-blue-700"
-              : "bg-green-100 text-green-700"
+              : session.status === "in-progress"
+              ? "text-blue-500"
+              : "text-green-500"
           }`}
         >
           <span className="font-medium text-gray-600">Status: </span>
